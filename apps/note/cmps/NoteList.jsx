82c5e13ef8b752bd825de266b3765d38d58495be
Dima <dima.aluf@gmail.com>
2025-02-22 18:13:27 +0200

    adding list cmp

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,3 +1,17 @@
-export function NoteList() {
-    return <div>note list</div>
+import { NotePreview } from './NotePreview.jsx'
+
+
+
+
+
+export function NoteList({ notes }) {
+
+    return (
+        <section className="note-list">
+            {notes.map(note =>
+                <li key={note.id}>
+                    <NotePreview note={note} />
+                </li>)}
+        </section>
+    )
 }
