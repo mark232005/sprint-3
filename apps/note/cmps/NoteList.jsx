e82e2264164fbb0c2540ec4ec23d55785f41ae78
Dima <dima.aluf@gmail.com>
2025-02-22 18:21:45 +0200

    adding note-item

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -9,7 +9,7 @@ export function NoteList({ notes }) {
     return (
         <section className="note-list">
             {notes.map(note =>
-                <li key={note.id}>
+                <li className="note-item" key={note.id}>
                     <NotePreview note={note} />
                 </li>)}
         </section>
