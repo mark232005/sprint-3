7825125641ed60c457a37f4248029ee676cccce5
Dima <dima.aluf@gmail.com>
2025-02-24 20:52:55 +0200

    delete node

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -4,14 +4,16 @@ import { NotePreview } from './NotePreview.jsx'
 
 
 
-export function NoteList({ notes , onRemoveNote}) {
+export function NoteList({ notes, onRemoveNote }) {
 
     return (
         <section className="note-list">
             {notes.map(note =>
                 <li className="note-item" key={note.id}>
-                    <NotePreview note={note} />
-                    
+                    <NotePreview
+                        note={note}
+                        onRemoveNote={onRemoveNote} />
+
                 </li>)}
         </section>
     )
