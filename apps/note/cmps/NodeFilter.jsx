2016ca3cd3ebe95819ccbd14fc79a6150dbbc300
Dima <dima.aluf@gmail.com>
2025-03-01 13:04:13 +0200

    last changes on filter by

--- apps/note/cmps/NodeFilter.jsx
+++ apps/note/cmps/NodeFilter.jsx
@@ -10,10 +10,11 @@ export function NodeFilter({ filter, onSetFilter }) {
 
     return (
         <section className="notes-filter-by-type">
-            <i className="fa-regular fa-message" onClick={() => filterClick('NoteTxt')}>  Text</i>
-            <i className="fa-regular fa-square-check" onClick={() => filterClick('NoteImg')}>  Image</i>
-            <i className="fa-regular fa-image" onClick={() => filterClick('NoteTodos')}>  Todos</i>
-            <div>{filter.type && <button onClick={() => filterClick('')}>All</button>}</div>
+            <h2>Filter by</h2>
+            <i className="fa-regular fa-message" onClick={() => filterClick('NoteTxt')}><span>Text</span></i>
+            <i className="fa-regular fa-square-check" onClick={() => filterClick('NoteImg')}><span>To Do</span></i>
+            <i className="fa-regular fa-image" onClick={() => filterClick('NoteTodos')}><span>Images</span></i>
+            {filter.type && <i class="fa-solid fa-arrow-rotate-left" onClick={() => filterClick('')}><span>All</span></i>}
         </section>
     )
 }
