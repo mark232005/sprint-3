5667f9ffb4a431d4718097dfa0a15df322b39652
mark <alufmark@gmail.com>
2025-03-03 23:42:53 +0200

    Add style to about us

--- pages/About.jsx
+++ pages/About.jsx
@@ -1,6 +1,20 @@
 
 export function About() {
-    return <section className="container about">
-        <h1>About Page</h1>
+    return <section className="container about ">
+        <h1 className="header-about"><strong>About Us:</strong></h1>
+        <div className="container-about grid">
+            <img className="my-img" src="assets/img/mark-img.jpg" />
+            <h2 className="my-name">Mark</h2>
+            <p className="about-me">I am only 20 years old from Ramla, currently working at the
+                airport before the army. In the past I developed and created many websites such as
+                the very famous game "Shula Makoshim" or a website
+                for creating memes and also a website called Misterblog</p>
+        </div>
+        <div className="container-about">
+            <img className="my-img" src="" />
+            <h2 className="my-name">Dima</h2>
+            <p className="about-me"></p>
+
+        </div>
     </section>
 }
