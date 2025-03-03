f4196698f778aa45dfce07efc08e42f5850d9d4d
Dima <dima.aluf@gmail.com>
2025-03-03 09:43:21 +0200

    add header

---
+++ apps/note/cmps/NoteMainFilterHeader.jsx
@@ -0,0 +1,11 @@
+import { noteService } from '../services/note.service.js'
+
+const { useState, useEffect, useRef } = React
+
+
+export function noteMainFilter () {
+    return (
+        <div className="note-main-filter">Hello</div>
+    )
+
+}
