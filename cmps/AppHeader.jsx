9180748a11bee8870df0acaa4552fc1aaf3f4331
Dima <dima.aluf@gmail.com>
2025-03-05 14:34:47 +0200

    add media query to app header

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -1,16 +1,29 @@
 const { Link, NavLink } = ReactRouterDOM
+const { useState } = React
 
 export function AppHeader() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    return <header className="app-header">
-        <Link to="/">
-            <h3>ALUF APPS</h3>
-        </Link>
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/mail">Mail</NavLink>
-            <NavLink to="/note">Note</NavLink>
-        </nav>
-    </header>
+    function toggleMenu() {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+    return (
+        <header className="app-header">
+            <Link to="/">
+                <h3>ALUF APPS</h3>
+            </Link>
+
+            <div className="nav-hamburger" onClick={toggleMenu}>
+                ☰
+            </div>
+
+            <nav className={isMenuOpen ? 'open' : ''}>
+                <NavLink to="/" onClick={() => setIsMenuOpen(false)}>Home</NavLink>
+                <NavLink to="/about" onClick={() => setIsMenuOpen(false)}>About</NavLink>
+                <NavLink to="/mail" onClick={() => setIsMenuOpen(false)}>Mail</NavLink>
+                <NavLink to="/note" onClick={() => setIsMenuOpen(false)}>Note</NavLink>
+            </nav>
+        </header>
+    )
 }
