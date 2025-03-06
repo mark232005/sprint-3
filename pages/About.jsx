b52a7356ef4b9fd4710ff794cf033301e3cdbd78
Dima <dima.aluf@gmail.com>
2025-03-06 12:23:22 +0200

    about me

--- pages/About.jsx
+++ pages/About.jsx
@@ -11,9 +11,13 @@ export function About() {
                 for creating memes and also a website called Misterblog</p>
         </div>
         <div className="container-about">
-            <img className="my-img" src="" />
+            <img className="my-img" src="assets/img/Dima-img.jpg" />
             <h2 className="my-name">Dima</h2>
-            <p className="about-me"></p>
+            <p className="about-me">I am a 33-year-old QA Automation Engineer with three years of experience at TechSee, an AI-driven company specializing in visual customer assistance technologies.
+                In my current role, I am responsible for designing and maintaining automated test frameworks, ensuring the delivery of high-quality and reliable products.
+
+                Alongside my work, I am actively expanding my technical skills, with a strong focus on deepening my understanding of software development and coding practices. 
+                My goal is to transition into a Full Stack Developer role within my company, contributing to both the development and quality processes to help drive innovation and product excellence.</p>
 
         </div>
     </section>
